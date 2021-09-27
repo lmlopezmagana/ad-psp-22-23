31f0298f4b3d7dc99eeeea60388721054d10e84e
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2021-09-27 14:28:06 +0200

    Ejercicio 02 Corregido. Incluye la colección de postman

---
+++ E02_Ejercicio/src/main/java/com/salesianostriana/dam/e02ejercicio/E02EjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.e02ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E02EjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E02EjercicioApplication.class, args);
+	}
+
+}
