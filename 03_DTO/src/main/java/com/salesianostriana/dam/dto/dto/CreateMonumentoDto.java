66ec4b43117cd8f6f4d2d23a7c02abc438fc6f38
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2021-09-28 14:27:35 +0200

    Ejercicio 02 Corregido. Incluye la colección de postman

---
+++ 03_DTO/src/main/java/com/salesianostriana/dam/dto/dto/CreateMonumentoDto.java
@@ -0,0 +1,22 @@
+package com.salesianostriana.dam.dto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class CreateMonumentoDto {
+
+    private String codigoPais;
+    private String pais;
+    private String ciudad;
+    private String loc;
+    private String nombre;
+    private String descripcion;
+    private String urlImagen;
+    private Long categoriaId;
+
+
+
+
+}
