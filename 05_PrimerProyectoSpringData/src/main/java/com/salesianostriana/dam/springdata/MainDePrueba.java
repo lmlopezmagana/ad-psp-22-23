6bab00b1703d9795bc64526cb5e8e3caad0e7b55
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2021-10-13 13:33:11 +0200

    Primer ejemplo Spring Data JPA

---
+++ 05_PrimerProyectoSpringData/src/main/java/com/salesianostriana/dam/springdata/MainDePrueba.java
@@ -0,0 +1,34 @@
+package com.salesianostriana.dam.springdata;
+
+import com.salesianostriana.dam.springdata.model.Alumno;
+import com.salesianostriana.dam.springdata.repos.AlumnoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class MainDePrueba {
+
+    private final AlumnoRepository repository;
+
+    @PostConstruct
+    public void test() {
+
+        Alumno alumno = Alumno.builder()
+                .nombre("Luismi")
+                .apellidos("López")
+                .email("luismi.lopez@salesianos.edu")
+                .build();
+
+        repository.save(alumno);
+
+        repository
+                .findAll()
+                .forEach(System.out::println);
+
+
+    }
+
+}
