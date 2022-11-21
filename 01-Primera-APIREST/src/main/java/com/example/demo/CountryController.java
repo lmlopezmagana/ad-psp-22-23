771a06bffa51166291c7bc1984b9ab2a051df4cc
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-11-21 11:00:50 +0100

    Ejemplo 01 completo

--- 01-Primera-APIREST/src/main/java/com/example/demo/CountryController.java
+++ 01-Primera-APIREST/src/main/java/com/example/demo/CountryController.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +15,7 @@ public class CountryController {
     private final CountryRepository repo;
 
     @GetMapping("/country/")
-    //public List<Country> findAll() {
     public ResponseEntity<List<Country>> findAll() {
-        //return repo.findAll();
         return ResponseEntity.ok(repo.findAll());
     }
 
@@ -28,4 +26,34 @@ public class CountryController {
         return ResponseEntity.of(repo.findFirstByCode(code));
     }
 
-}
+    @PostMapping("/country/")
+    public ResponseEntity<Country> newCountry(@RequestBody Country country) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(repo.save(country));
+    }
+
+    @PutMapping("/country/{id}")
+    public ResponseEntity<Country> editCountry(@RequestBody Country country,
+                                               @PathVariable Long id) {
+        return ResponseEntity.of(
+                repo.findById(id)
+                        .map(old -> {
+                            old.setCapital(country.getCapital());
+                            old.setCode(country.getCode());
+                            old.setName(country.getName());
+                            old.setCurrency(country.getCurrency());
+                            old.setPopulation(country.getPopulation());
+                            return Optional.of(repo.save(old));
+                        })
+                        .orElse(Optional.empty())
+        );
+    }
+
+        @DeleteMapping("/country/{id}")
+        public ResponseEntity<?> deleteCountry (@PathVariable Long id){
+            if (repo.existsById(id))
+                repo.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+    }
